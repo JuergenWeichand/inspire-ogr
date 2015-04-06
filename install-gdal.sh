0a8444838246e3dcd3a6e485611bc6c15178b85b
Jürgen Weichand <juergen@weichand.de>
2015-04-06 17:27:02 +0200

    travisci fix

--- install-gdal.sh
+++ install-gdal.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 gdal_version=1.11.2
-mkdir /tmp/
 cd /tmp/
 wget http://download.osgeo.org/gdal/$gdal_version/gdal-$gdal_version.tar.gz
 tar xvzf gdal-$gdal_version.tar.gz
@@ -8,4 +7,5 @@ cd gdal-$gdal_version
 ./configure
 make
 make install
+ldconfig
 cd
